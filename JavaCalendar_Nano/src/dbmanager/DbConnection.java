22cd9f7a41d572ce6eaa79b0089a6508d08435fe
ineshKCWA <ineshgunasekara@ymail.com>
2015-06-17 02:34:19 +0530

    add new variables for connection

--- JavaCalendar_Nano/src/dbmanager/DbConnection.java
+++ JavaCalendar_Nano/src/dbmanager/DbConnection.java
@@ -1,5 +1,14 @@
 package dbmanager;
 
-public class DbConnection {
 
+import java.sql.Connection;
+
+public class DbConnection {
+	private static DbConnection instance=null;
+	
+	private static final String USERNAME = "teamnano";
+	private static final String PASSWORD = "nano";
+	private static final String CONN_STRING =
+			"jdbc:mysql://localhost/calendardb";
+	private Connection conn=null;
 }
