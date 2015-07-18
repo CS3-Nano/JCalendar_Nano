626437ad789e335cecb5f226cee1e4dbe956597c
Inesh <Inesh@IDG-PC>
2015-07-19 02:03:04 +0530

    new methods for get customize colors

--- JavaCalendar_Nano/src/service/UsrCustmizService.java
+++ JavaCalendar_Nano/src/service/UsrCustmizService.java
@@ -26,5 +26,58 @@ public class UsrCustmizService {
 		}
 		return UsrCstmzMngr.customize.get(0);
 	}
-	
+	public String getHighPrtyClr(String val){
+		int i=Integer.parseInt(val);
+		switch (i) {
+		case 1:
+			return "darkorange";
+		case 2:
+			return "darksalmon";		
+		case 3:
+			return "firebrick";
+		case 4:
+			return "deeppink";
+		case 5:
+			return "darkgoldenrod";
+		default:
+			return "darksalmon";
+			
+		}
+	}
+	public String getMedPrtyClr(String val){
+		int i=Integer.parseInt(val);
+		switch (i) {
+		case 1:
+			return "darkcyan";
+		case 2:
+			return "dodgerblue";		
+		case 3:
+			return "deepskyblue";
+		case 4:
+			return "aqua";
+		case 5:
+			return "aquamarine";
+		default:
+			return "aqua";
+			
+		}
+	}
+	public String getLowPrtyClr(String val){
+		int i=Integer.parseInt(val);
+		switch (i) {
+		case 1:
+			return "darkslategray";
+		case 2:
+			return "darkgrey";		
+		case 3:
+			return "antiquewhite";
+		case 4:
+			return "bisque";
+		case 5:
+			return "aquamarine";
+		default:
+			return "burlywood";
+			
+		}
+	}
 }
